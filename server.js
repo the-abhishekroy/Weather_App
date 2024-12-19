55df2988fef8c01e91e29532c8a975f264a10f94
ABHISHEK <harshroy0501@gmail.com>
2024-12-19 17:22:17 +0530

    Final Changes

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { weather: null, error: null });
+});
+
+app.get("/weather", async (req, res) => {
+  const city = req.query.city;
+  const apiKey = "3938e34b9a43af2092c9884d0d7e0f99";
+
+  const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  let weather;
+  let error = null;
+  try {
+    const response = await axios.post(APIUrl);
+    weather = response.data;
+  } catch (error) {
+    weather = null;
+    error = "Error, Please try again";
+  }
+  res.render("index", { weather, error });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
